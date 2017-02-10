2a24e6018bd839c50369cacffd4ceaa236347446
Acs Attila <Acs Attila>
2017-02-10 18:38:06 +0100

    Add project files.

---
+++ eventus-tasklist-design/Models/TasklistItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+
+namespace eventus_tasklist_design.Models
+{
+    public class TasklistItem
+    {
+        public string TicketId { get; set; } = "123456";
+        public string TaskNumber { get; set; } = "12";
+        public string TaskName { get; set; } = "Nagyon hosszu fajlnev, ami amit az angol billentyuzeten nem tudok leirni rendesen";
+        public string Priority { get; set; } = "100";
+        public Color PriorityColor { get; set; } = Color.FromArgb(255, 244, 67, 54);
+        public string GroupName { get; set; } = "Sanyika groupja";
+        public string ImageSource { get; set; } = "";
+        public string Executor { get; set; } = "Acs attila marinénike kiskutyája";
+        public string DueDate { get; set; } = "10.17";
+        public string DueDateTime { get; set; } = "12:12";
+        public string Distance { get; set; } = "123";
+        public Visibility IsSaved { get; set; } = Visibility.Collapsed;
+        public Visibility IsNew { get; set; } = Visibility.Collapsed;
+        public Visibility IsLocked { get; set; } = Visibility.Collapsed;
+        public Visibility IsError { get; set; } = Visibility.Collapsed;
+    }
+}
---
+++ eventus-tasklist-design/ViewModels/TaskListPageViewModel.cs
@@ -0,0 +1,46 @@
+﻿using eventus_tasklist_design.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml;
+
+namespace eventus_tasklist_design.ViewModels
+{
+    public class TaskListPageViewModel
+    {
+        public List<TasklistItem> Tasks {
+            get => new List<TasklistItem> {
+                new TasklistItem{
+                    IsError = Visibility.Visible,
+                    PriorityColor = Color.FromArgb(255,76,175,80)
+                },
+                new TasklistItem{
+                    IsLocked = Visibility.Visible,
+                    PriorityColor = Color.FromArgb(255,255,235,59)
+                },
+                new TasklistItem{
+                    IsNew = Visibility.Visible,
+                    PriorityColor = Color.FromArgb(255,255,152,0)
+                },
+                new TasklistItem{
+                    IsSaved = Visibility.Visible
+                },
+                new TasklistItem{
+                    IsSaved = Visibility.Visible,
+                    IsError = Visibility.Visible,
+                    PriorityColor = Color.FromArgb(255,244,67,54)
+                },
+                new TasklistItem{
+                    IsSaved = Visibility.Visible,
+                    IsError = Visibility.Visible,
+                    IsLocked = Visibility.Visible,
+                    PriorityColor = Color.FromArgb(255,255,193,7),
+                    TaskName = "Nagyon hosszu fajlnev, ami amit az angol billentyuzeten nem tudok leirni rendesen Nagyon hosszu fajlnev, ami amit az angol billentyuzeten nem tudok leirni rendesen Nagyon hosszu fajlnev, ami amit az angol billentyuzeten nem tudok leirni rendesen"
+                },
+            };
+        }
+    }
+}
